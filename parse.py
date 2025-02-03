95b12635176798099904668b2d28b76609e20df3
WRXer <wrxerrr@gmail.com>
2025-02-03 17:20:40 +0400

    Добавил вывод кф по п1, Х, п2

--- parse.py
+++ parse.py
@@ -1,7 +1,7 @@
 import requests
-import json
 
-sent_events = {}    # Словарь для отслеживания отправленных событий
+
+sent_events = {}    #Словарь для отслеживания отправленных событий
 
 
 def fetch_and_display_events():
@@ -24,30 +24,35 @@ def fetch_and_display_events():
     )
     data = response.json()
 
-    events = data.get('events', [])
-
-    with open('dump.json', 'w', encoding='utf-8') as f:     #Сохранение результата парсинга
-        json.dump(data, f, ensure_ascii=False, indent=4)
+    events = data.get('events', [])    #Список событий
+    custom_factors = data.get('customFactors', [])    #Список коэффициентов
 
     filtered_events = [
         event for event in events
         if event.get('place') == 'live' and
            event.get('sportId') in sport_ids and
            event.get('level') == 1
-    ]   # Фильтруем нужные нам события
+    ]   #Фильтруем нужные нам события
 
     new_events = [
         event for event in filtered_events
         if event['id'] not in sent_events
-    ]   # Добавляем новые события в отправленные, чтобы не повторялась отправка
-
+    ]   #Добавляем новые события в отправленные, чтобы не повторялась отправка
+    print(new_events)
     if new_events:
         message_matches = ""
         for event in new_events:
-            message_matches += (
-                f"{event.get('team1')} - {event.get('team2')}\n"
-            )
-            sent_events[event['id']] = event    # Отмечаем событие как отправленное
+            event_name = f"{event.get('team1')} - {event.get('team2')}"
+            event_factors = next((info for info in custom_factors if info.get('e') == event['id']), None)    #Поиск коэффициентов для данного события
+            if event_factors:
+                factors = {factor['f']: factor['v'] for factor in event_factors.get('factors', [])}
+                p1 = factors.get(921, "—")  # П1
+                x = factors.get(922, "—")  # Х
+                p2 = factors.get(923, "—")  # П2
+                message_matches += f"{event_name}: П1 {p1}  |  X {x}  |  П2 {p2}\n"
+
+
+            sent_events[event['id']] = event  #Отмечаем событие как отправленное
         return message_matches
     else:
         return None
